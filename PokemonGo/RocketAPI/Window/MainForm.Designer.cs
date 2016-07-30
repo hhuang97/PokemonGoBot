2b68725fe67ccc310d1db7832fb74d1a8d33528f
huanghansong <hansong.huang@yahoo.com>
2016-07-30 17:54:50 -0500

    working bot upload

---
+++ PokemonGo/RocketAPI/Window/MainForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace PokemonGo.RocketAPI.Window
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.showAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showAllToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnForceUnban = new System.Windows.Forms.Button();
+            this.btnLuckyEgg = new System.Windows.Forms.Button();
+            this.btnStartFarming = new System.Windows.Forms.Button();
+            this.btnadvoptions = new System.Windows.Forms.Button();
+            this.MainMap = new GMap.NET.WindowsForms.GMapControl();
+            this.dGrid = new System.Windows.Forms.DataGridView();
+            this.btnPokemon = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.Color.Black;
+            this.logTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logTextBox.ForeColor = System.Drawing.Color.White;
+            this.logTextBox.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.logTextBox.Location = new System.Drawing.Point(225, 285);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(468, 207);
+            this.logTextBox.TabIndex = 0;
+            this.logTextBox.Text = "";
+            this.logTextBox.TextChanged += new System.EventHandler(this.logTextBox_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 495);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(701, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(39, 17);
+            this.statusLabel.Text = "Status";
+            // 
+            // showAllToolStripMenuItem
+            // 
+            this.showAllToolStripMenuItem.Name = "showAllToolStripMenuItem";
+            this.showAllToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // showAllToolStripMenuItem1
+            // 
+            this.showAllToolStripMenuItem1.Name = "showAllToolStripMenuItem1";
+            this.showAllToolStripMenuItem1.Size = new System.Drawing.Size(32, 19);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnForceUnban);
+            this.groupBox1.Controls.Add(this.btnLuckyEgg);
+            this.groupBox1.Controls.Add(this.btnStartFarming);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(201, 135);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Farming Control";
+            // 
+            // btnForceUnban
+            // 
+            this.btnForceUnban.Location = new System.Drawing.Point(6, 103);
+            this.btnForceUnban.Name = "btnForceUnban";
+            this.btnForceUnban.Size = new System.Drawing.Size(189, 23);
+            this.btnForceUnban.TabIndex = 16;
+            this.btnForceUnban.Text = "Force Unban";
+            this.btnForceUnban.UseVisualStyleBackColor = true;
+            this.btnForceUnban.Click += new System.EventHandler(this.btnForceUnban_Click);
+            // 
+            // btnLuckyEgg
+            // 
+            this.btnLuckyEgg.Location = new System.Drawing.Point(6, 77);
+            this.btnLuckyEgg.Name = "btnLuckyEgg";
+            this.btnLuckyEgg.Size = new System.Drawing.Size(189, 23);
+            this.btnLuckyEgg.TabIndex = 7;
+            this.btnLuckyEgg.Text = "Use Lucky egg";
+            this.btnLuckyEgg.UseVisualStyleBackColor = true;
+            this.btnLuckyEgg.Click += new System.EventHandler(this.btnLuckyEgg_Click);
+            // 
+            // btnStartFarming
+            // 
+            this.btnStartFarming.Location = new System.Drawing.Point(6, 22);
+            this.btnStartFarming.Name = "btnStartFarming";
+            this.btnStartFarming.Size = new System.Drawing.Size(189, 49);
+            this.btnStartFarming.TabIndex = 0;
+            this.btnStartFarming.Text = "Start Farming";
+            this.btnStartFarming.UseVisualStyleBackColor = true;
+            this.btnStartFarming.Click += new System.EventHandler(this.btnStartFarming_Click);
+            // 
+            // btnadvoptions
+            // 
+            this.btnadvoptions.Location = new System.Drawing.Point(12, 469);
+            this.btnadvoptions.Name = "btnadvoptions";
+            this.btnadvoptions.Size = new System.Drawing.Size(201, 23);
+            this.btnadvoptions.TabIndex = 15;
+            this.btnadvoptions.Text = "Advanced Options";
+            this.btnadvoptions.UseVisualStyleBackColor = true;
+            this.btnadvoptions.Click += new System.EventHandler(this.btnadvoptions_Click);
+            // 
+            // MainMap
+            // 
+            this.MainMap.Bearing = 0F;
+            this.MainMap.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainMap.CanDragMap = true;
+            this.MainMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.MainMap.Enabled = false;
+            this.MainMap.GrayScaleMode = false;
+            this.MainMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.MainMap.LevelsKeepInMemmory = 5;
+            this.MainMap.Location = new System.Drawing.Point(12, 153);
+            this.MainMap.MarkersEnabled = true;
+            this.MainMap.MaxZoom = 2;
+            this.MainMap.MinZoom = 2;
+            this.MainMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MainMap.Name = "MainMap";
+            this.MainMap.NegativeMode = false;
+            this.MainMap.PolygonsEnabled = true;
+            this.MainMap.RetryLoadTile = 0;
+            this.MainMap.RoutesEnabled = true;
+            this.MainMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.MainMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.MainMap.ShowTileGridLines = false;
+            this.MainMap.Size = new System.Drawing.Size(201, 281);
+            this.MainMap.TabIndex = 16;
+            this.MainMap.Zoom = 0D;
+            // 
+            // dGrid
+            // 
+            this.dGrid.AllowUserToAddRows = false;
+            this.dGrid.AllowUserToDeleteRows = false;
+            this.dGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGrid.Location = new System.Drawing.Point(225, 12);
+            this.dGrid.Name = "dGrid";
+            this.dGrid.ReadOnly = true;
+            this.dGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dGrid.Size = new System.Drawing.Size(464, 267);
+            this.dGrid.TabIndex = 0;
+            // 
+            // btnPokemon
+            // 
+            this.btnPokemon.Enabled = false;
+            this.btnPokemon.Location = new System.Drawing.Point(12, 440);
+            this.btnPokemon.Name = "btnPokemon";
+            this.btnPokemon.Size = new System.Drawing.Size(201, 23);
+            this.btnPokemon.TabIndex = 17;
+            this.btnPokemon.Text = "Pokemon";
+            this.btnPokemon.UseVisualStyleBackColor = true;
+            this.btnPokemon.Click += new System.EventHandler(this.btnPokemon_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 517);
+            this.Controls.Add(this.btnPokemon);
+            this.Controls.Add(this.dGrid);
+            this.Controls.Add(this.MainMap);
+            this.Controls.Add(this.btnadvoptions);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.statusStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(717, 556);
+            this.MinimumSize = new System.Drawing.Size(717, 556);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PoGoBoT - GUI";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox logTextBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.ToolStripMenuItem showAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showAllToolStripMenuItem1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLuckyEgg;
+        private System.Windows.Forms.Button btnStartFarming;
+        private System.Windows.Forms.Button btnadvoptions;
+        private System.Windows.Forms.Button btnForceUnban;
+        private GMap.NET.WindowsForms.GMapControl MainMap;
+        private System.Windows.Forms.DataGridView dGrid;
+        private System.Windows.Forms.Button btnPokemon;
+    }
+}
---
+++ PokemonGo/RocketAPI/Window/SettingsForm.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.MapProviders;
+using GMap.NET;
+using System.Configuration;
+using System.Globalization;
+
+namespace PokemonGo.RocketAPI.Window
+{
+    partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        public class Loc
+        {
+            public string name { get; set; }
+            public double lat { get; set; }
+            public double lng { get; set; }
+
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+
+        private void addfarminglocations()
+        {
+            comboLocations.Items.Add(new Loc() { name = "London, England", lat = 51.501663, lng = -0.14102 });
+            comboLocations.Items.Add(new Loc() { name = "Myrtle Beach, SC, USA", lat = 33.714451, lng = -78.877194 });
+            comboLocations.Items.Add(new Loc() { name = "Santa Monica Pier, LA, USA", lat = 34.00873594425199, lng = -118.49761247634888 });
+            comboLocations.Items.Add(new Loc() { name = "Long Beach, CA, USA", lat = 34.00873594425199, lng = -118.49761247634888 });
+            comboLocations.Items.Add(new Loc() { name = "Seattle, WA, USA", lat = 47.626680, lng = -122.335884 });
+            comboLocations.Items.Add(new Loc() { name = "Bryant Park, NY, USA", lat = 40.75320648472645, lng = -73.98390769958496 });
+            comboLocations.Items.Add(new Loc() { name = "Vancouver, BC, Canada", lat = 45.6298963412979, lng = -122.67196521162987 });
+            comboLocations.Items.Add(new Loc() { name = "Sydney, Australia", lat = -33.870273020353416, lng = 151.20878219604492 });
+            comboLocations.Items.Add(new Loc() { name = "Achen, Germany", lat = 50.776309, lng = 6.083505 });
+            comboLocations.Items.Add(new Loc() { name = "Ottawa, Canada", lat = 45.42158812329091, lng = -75.6877326965332 });
+            comboLocations.Items.Add(new Loc() { name = "Hamburg, Germany", lat = 53.5588061, lng = 10.057689399999958 });
+            comboLocations.Items.Add(new Loc() { name = "Dusseldorf, Germany", lat = 51.224382, lng = 6.778896 });
+            comboLocations.Items.Add(new Loc() { name = "Tokyo, Japan", lat = 35.69051125265253, lng = 139.68954205513 });
+            comboLocations.Items.Add(new Loc() { name = "Disneyland Park", lat = 33.8120962, lng = -117.9189742 });
+        }
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+            authTypeCb.Text = Settings.Instance.AuthType.ToString();
+            ptcUserText.Text = Settings.Instance.PtcUsername.ToString();
+            ptcPassText.Text = Settings.Instance.PtcPassword.ToString();
+            EmailLoginBox.Text = Settings.Instance.Email.ToString();
+            EmailPasswordBox.Text = Settings.Instance.Password.ToString();
+            latitudeText.Text = Settings.Instance.DefaultLatitude.ToString();
+            longitudeText.Text = Settings.Instance.DefaultLongitude.ToString();
+            razzmodeCb.Text = Settings.Instance.RazzBerryMode;
+            razzSettingText.Text = Settings.Instance.RazzBerrySetting.ToString();
+            transferTypeCb.Text = Settings.Instance.TransferType;
+            transferCpThresText.Text = Settings.Instance.TransferCPThreshold.ToString();
+            transferIVThresText.Text = Settings.Instance.TransferIVThreshold.ToString();
+            evolveAllChk.Checked = Settings.Instance.EvolveAllGivenPokemons;
+            CatchPokemonBox.Checked = Settings.Instance.CatchPokemon;
+            TravelSpeedBox.Text = Settings.Instance.TravelSpeed.ToString();
+            ImageSizeBox.Text = Settings.Instance.ImageSize.ToString();
+            // Initialize map:
+            //use google provider
+            gMapControl1.MapProvider = GoogleMapProvider.Instance;
+            //get tiles from server only
+            gMapControl1.Manager.Mode = AccessMode.ServerOnly;
+            //not use proxy
+            GMapProvider.WebProxy = null;
+            //center map on moscow
+            string lat = ConfigurationManager.AppSettings["DefaultLatitude"];
+            string longit = ConfigurationManager.AppSettings["DefaultLongitude"];
+            lat.Replace(',', '.');
+            longit.Replace(',', '.');
+            gMapControl1.Position = new PointLatLng(double.Parse(lat.Replace(",", "."), CultureInfo.InvariantCulture), double.Parse(longit.Replace(",", "."), CultureInfo.InvariantCulture));
+
+
+
+            //zoom min/max; default both = 2
+            gMapControl1.DragButton = MouseButtons.Left;
+
+            gMapControl1.CenterPen = new Pen(Color.Red, 2);
+            gMapControl1.MinZoom = trackBar.Maximum = 1;
+            gMapControl1.MaxZoom = trackBar.Maximum = 20;
+            trackBar.Value = 10;
+
+            //set zoom
+            gMapControl1.Zoom = trackBar.Value;
+
+            //Add Options
+            addfarminglocations();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            Settings.Instance.SetSetting(authTypeCb.Text, "AuthType");
+            Settings.Instance.SetSetting(EmailLoginBox.Text, "Email");
+            Settings.Instance.SetSetting(EmailPasswordBox.Text, "Password");
+            Settings.Instance.SetSetting(ptcUserText.Text, "PtcUsername");
+            Settings.Instance.SetSetting(ptcPassText.Text, "PtcPassword");
+            Settings.Instance.SetSetting(latitudeText.Text.Replace(',', '.'), "DefaultLatitude");
+            Settings.Instance.SetSetting(longitudeText.Text.Replace(',', '.'), "DefaultLongitude");
+
+            string lat = ConfigurationManager.AppSettings["DefaultLatitude"];
+            string longit = ConfigurationManager.AppSettings["DefaultLongitude"];
+            lat.Replace(',', '.');
+            longit.Replace(',', '.');
+
+
+            Settings.Instance.SetSetting(razzmodeCb.Text, "RazzBerryMode");
+            Settings.Instance.SetSetting(razzSettingText.Text, "RazzBerrySetting");
+            Settings.Instance.SetSetting(transferTypeCb.Text, "TransferType");
+            Settings.Instance.SetSetting(transferCpThresText.Text, "TransferCPThreshold");
+            Settings.Instance.SetSetting(transferIVThresText.Text, "TransferIVThreshold");
+            Settings.Instance.SetSetting(TravelSpeedBox.Text, "TravelSpeed");
+            Settings.Instance.SetSetting(ImageSizeBox.Text, "ImageSize");
+            Settings.Instance.SetSetting(evolveAllChk.Checked ? "true" : "false", "EvolveAllGivenPokemons");
+            Settings.Instance.SetSetting(CatchPokemonBox.Checked ? "true" : "false", "CatchPokemon");
+            Settings.Instance.Reload();
+            Close();
+        }
+
+        private void authTypeCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (authTypeCb.Text == "google")
+            {
+                EmailLoginBox.Visible = true;
+                EmailLoginText.Visible = true;
+                EmailPasswordBox.Visible = true;
+                EmailPasswordText.Visible = true;
+                ptcUserText.Visible = false;
+                ptcPassText.Visible = false;
+                ptcUserLabel.Visible = false;
+                ptcPasswordLabel.Visible = false;
+            }
+            else
+            {
+                EmailLoginBox.Visible = false;
+                EmailLoginText.Visible = false;
+                EmailPasswordBox.Visible = false;
+                EmailPasswordText.Visible = false;
+                ptcUserText.Visible = true;
+                ptcPassText.Visible = true;
+                ptcUserLabel.Visible = true;
+                ptcPasswordLabel.Visible = true;
+            }
+        }
+
+        private void gMapControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point localCoordinates = e.Location;
+            gMapControl1.Position = gMapControl1.FromLocalToLatLng(localCoordinates.X, localCoordinates.Y);
+
+            if (e.Clicks >= 2)
+            {
+                gMapControl1.Zoom += 5;
+            }
+
+            double X = Math.Round(gMapControl1.Position.Lng, 6);
+            double Y = Math.Round(gMapControl1.Position.Lat, 6);
+            string longitude = X.ToString();
+            string latitude = Y.ToString();
+            latitudeText.Text = latitude;
+            longitudeText.Text = longitude;
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            gMapControl1.Zoom = trackBar.Value;
+        }
+
+
+        private void authTypeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void transferCpThresText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void transferTypeCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (transferTypeCb.Text == "CP")
+            {
+                label4.Visible = true;
+                transferCpThresText.Visible = true;
+            }
+            else
+            {
+                label4.Visible = false;
+                transferCpThresText.Visible = false;
+
+            }
+
+            if (transferTypeCb.Text == "IV")
+            {
+                label6.Visible = true;
+                transferIVThresText.Visible = true;
+            }
+            else
+            {
+                label6.Visible = false;
+                transferIVThresText.Visible = false;
+
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FindAdressButton_Click_1(object sender, EventArgs e)
+        {
+            latitudeText.Text = Convert.ToString(gMapControl1.Position.Lat);
+            longitudeText.Text = Convert.ToString(gMapControl1.Position.Lat);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void evolveAllChk_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboLocations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double lat;
+            double lng;
+
+            Loc location = (Loc)comboLocations.SelectedItem;
+            latitudeText.Text = location.lat.ToString();
+            longitudeText.Text = location.lng.ToString();
+
+            lat = location.lat;
+            lng = location.lng;
+
+
+            gMapControl1.Position = new GMap.NET.PointLatLng(lat, lng);
+        }
+
+        private void FindAdressButton_Click(object sender, EventArgs e)
+        {
+            gMapControl1.SetPositionByKeywords(AdressBox.Text);
+            gMapControl1.Zoom = 15;
+            double X = Math.Round(gMapControl1.Position.Lng, 6);
+            double Y = Math.Round(gMapControl1.Position.Lat, 6);
+            string longitude = X.ToString();
+            string latitude = Y.ToString();
+            latitudeText.Text = latitude;
+            longitudeText.Text = longitude;
+        }
+    }
+}
